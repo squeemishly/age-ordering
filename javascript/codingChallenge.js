93ea78a47db0ccd5a4ebb641ce0a6a763e0013b7
squeemishly <squeemishly@gmail.com>
2017-10-01 16:20:42 -0600

    refactors sorting logic into their own functions for SRP

--- javascript/codingChallenge.js
+++ javascript/codingChallenge.js
@@ -1,13 +1,20 @@
 class AgeOrdering {
   namify(arr) {
-    const sorted = arr.sort( (a, b) => {
-      if (a[1] < b[1]) return -1
-      if (a[1] > b[1]) return 1;
-      return 0
-    })
-    return sorted.map(person => {
+    return this.sortMaster(arr).map(person => {
       return person[0]
     })
+  }
+
+  sortMaster(arr) {
+    return arr.sort( (a, b) => {
+      return this.sortify(a, b)
+    })
+  }
+
+  sortify(a, b) {
+    if (a[1] < b[1]) return -1
+    if (a[1] > b[1]) return 1;
+    return 0
   }
 }
 
