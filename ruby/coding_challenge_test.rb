413b91c6475d96def3b27b37684f8a8eaab1e9b9
squeemishly <squeemishly@gmail.com>
2017-10-01 13:58:27 -0600

    writes the test to return the name of one person

--- ruby/coding_challenge_test.rb
+++ ruby/coding_challenge_test.rb
@@ -3,14 +3,26 @@ require 'pry'
 require 'minitest/autorun'
 require_relative './coding_challenge'
 
-class ObjectTest < Minitest::Test
-  attr_reader :template
+class AgeOrderingTest < Minitest::Test
+  attr_reader :age_ordering
 
   def setup
-    @template = TemplateClass.new
+    @age_ordering = AgeOrdering.new
+    data = [
+            ['Frank', 33],
+            ['Stacy', 15],
+            ['Juan', 24],
+            ['Dom', 32],
+            ['Steve', 24],
+            ['Jill', 24]
+          ]
   end
 
   def test_it_exists
-    assert_instance_of TemplateClass, template
+    assert_instance_of AgeOrdering, age_ordering
+  end
+
+  def test_it_returns_the_name_of_one_person
+    assert_equal "Boudi", age_ordering.names([["Boudi", 12]])
   end
 end
