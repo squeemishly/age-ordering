0abc1abb472bc5783f690f089071a38ee0dee273
squeemishly <squeemishly@gmail.com>
2017-10-01 15:04:00 -0600

    moved sorting to it's own method and made it private

--- ruby/coding_challenge.rb
+++ ruby/coding_challenge.rb
@@ -2,23 +2,25 @@ require 'pry'
 
 class AgeOrdering
   def names(arr)
-    sorted = arr.sort { |x, y| x[1] <=> y[1] }
-    sorted.map do |name|
+    sortify(arr).map do |name|
       name[0]
     end
   end
 
   def name_and_ages(arr)
-    sorted = arr.sort { |x, y| x[1] <=> y[1] }
-    sorted.map do |person|
+    sortify(arr).map do |person|
       "#{person[0]} (#{person[1]})"
     end
   end
 
   def hashify(arr)
-    sorted = arr.sort { |x, y| x[1] <=> y[1] }
-    sorted.each_with_object( {} ) do |person, result|
+    sortify(arr).each_with_object( {} ) do |person, result|
       (result[person[1]] ||= []) << person[0]
     end
+  end
+
+  private
+  def sortify(arr)
+    arr.sort { |x, y| x[1] <=> y[1] }
   end
 end
